7e9ba181e279c7573c26186e062fa908261d5350
Saarthak <saarthakgupta08@gmail.com>
2019-07-08 09:37:35 +0530

    Added Project Files

---
+++ src/main/java/com/sg/KafkaPublisher/KafkaPublisherApplication.java
@@ -0,0 +1,30 @@
+package com.sg.KafkaPublisher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class KafkaPublisherApplication {
+
+	@Autowired
+	KafkaTemplate<String,Object> template;
+
+	private String topic="testing-topic";
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPublisherApplication.class, args);
+	}
+
+
+	@GetMapping("/publish/{name}")
+	public String publishMessage(@PathVariable String name){
+		template.send(topic,"Hi! Testing 123..!!");
+		return "Data-Published";
+	}
+}
